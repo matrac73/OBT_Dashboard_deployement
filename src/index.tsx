cc315b0e5bfa00748c3fce386c9adae3ec43da6a
mathieu.ract <mathieu.ract@student-cs.fr>
2022-10-27 18:30:37 +0200

    try

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/callpage" element={<CallPage />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<App />}>
+      <Route path="/" element={<Login />}>
         <Route path="/" element={<Dashboard />} />
       </Route>
     </Routes>
